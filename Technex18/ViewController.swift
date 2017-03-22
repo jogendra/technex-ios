9af5c6d37785880b275453101ad7c4e324eb379d
imjog <imjog24@gmail.com>
2017-03-22 23:34:25 +0530

    Initial Commit

---
+++ Technex18/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Technex18
+//
+//  Created by JOGENDRA on 22/03/17.
+//  Copyright © 2017 Jogendra Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
